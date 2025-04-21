1cb0551c11996645f958677c6deb22972b5b9f2a
AnnabelleMin <113308586+AnnabelleMin@users.noreply.github.com>
2025-04-21 17:19:16 -0400

    Update finetune.sh

--- finetune.sh
+++ finetune.sh
@@ -1,27 +1,24 @@
-# (re‑login if needed)
+# re‑login wandb if you need to
 wandb login
 
-# launch with BF16, 4‑bit QLoRA, FlashAttention and ZeRO‑2 offload
 accelerate launch \
   --mixed_precision bf16 \
   --deepspeed_config_file ds_config.json \
   finetune.py \
-    --data_path datasets/SAIL_train_deberta_entailment.json \
-    --run_name "test_1" \
-    --output_dir ./llama2-lora-entail \
-    --lora_r 16 \
-    --lora_alpha 32 \
-    --lora_dropout 0.1 \
-    --load_in_4bit \
-    --bnb_4bit_quant_type nf4 \
-    --use_xformers_memory_efficient_attention \
-    --num_train_epochs 1 \
+    --data_path               datasets/SAIL_train_deberta_entailment.json \
+    --run_name                "test_1" \
+    --output_dir              ./llama2-lora-entail \
+    --lora_r                  16 \
+    --lora_alpha              32 \
+    --lora_dropout            0.1 \
+    --qlora \
+    --num_train_epochs        1 \
     --per_device_train_batch_size 2 \
-    --per_device_eval_batch_size 1 \
+    --per_device_eval_batch_size  1 \
     --gradient_accumulation_steps 16 \
-    --learning_rate 2e-5 \
-    --warmup_ratio 0.03 \
-    --lr_scheduler_type cosine \
-    --save_steps 400 \
-    --save_total_limit 10 \
-    --model_max_length 512
+    --learning_rate           2e-5 \
+    --warmup_ratio            0.03 \
+    --lr_scheduler_type       cosine \
+    --save_steps              400 \
+    --save_total_limit        10 \
+    --model_max_length        512
